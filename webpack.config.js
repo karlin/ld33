46978269ba9eaa99737838ed24f000e7f0b280ce
karlin <karlin.fox@modustri.com>
2015-08-23 17:30:17 -0400

    Remove hot loading from webpack entry

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var webpack = require('webpack');
 
 module.exports = {
   entry: {
-    app: ['webpack/hot/dev-server', './app/index.js'],
+    app: ['./app/index.js'],
   },
   output: {
     path: path.join(__dirname, 'dist'),
