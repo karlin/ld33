c0b0db97994f78c0808974e7c8386947961e3854
karlin <karlin.fox@modustri.com>
2015-08-23 17:18:45 -0400

    clean up some webpack conf

--- webpack.config.js
+++ webpack.config.js
@@ -4,21 +4,15 @@ var webpack = require('webpack');
 module.exports = {
   entry: {
     app: ['webpack/hot/dev-server', './app/index.js'],
-    // vendor: ['processing-js', 'toxiclibsjs', 'underscore']
   },
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js'
   },
-  // plugins: [
-    // new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.bundle.js')
-  // ],
-  // plugins: [new webpack.optimize.UglifyJsPlugin()],
+  plugins: [new webpack.optimize.UglifyJsPlugin()],
   module: {
     loaders: [
       { test: /\.css$/, loader: 'style!css' },
-      // { test: require.resolve('toxiclibsjs'), loader: 'expose?toxi' },
-      // { test: require.resolve('jquery'), loader: 'expose?jQuery' },
       {
         test: /\.js$/,
         include: [
@@ -30,7 +24,7 @@ module.exports = {
     ],
     noParse: [
       path.join(__dirname, 'vendor', 'toxiclibs.min.js'),
-      path.join(__dirname, 'vendor', 'processing.min.js'),
+      path.join(__dirname, 'vendor', 'processing.min.js')
     ]
   },
   resolve: {
